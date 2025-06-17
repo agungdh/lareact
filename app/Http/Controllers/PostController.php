8ec61a94e4ca8f6ff23c67c3324a64626234c628
AgungDH <agungdh@live.com>
2025-06-17 22:39:35 +0700

    Fix formatting issues in test files and remove unused import in PostSeeder

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,11 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
-use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         return inertia('post/index', [
             'posts' => Post::all(),
         ]);
