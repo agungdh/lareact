d9cbf20fb58651cf1c433fe4a378e7845ab78488
AgungDH <agungdh@live.com>
2025-06-19 18:41:46 +0700

    Add Category and Tag models, factories, and seeders with migrations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            CategorySeeder::class,
+            TagSeeder::class,
             PostSeeder::class,
         ]);
 
