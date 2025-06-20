b33968940c96aaa3410900c24c2e877b915242cb
AgungDH <agungdh@live.com>
2025-06-20 14:53:59 +0700

    Refactor Post model factory usage annotation for clarity

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Database\Factories\PostFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -9,7 +10,7 @@ use Laravel\Scout\Searchable;
 
 class Post extends Model
 {
-    /** @use HasFactory<\Database\Factories\PostFactory> */
+    /** @use HasFactory<PostFactory> */
     use HasFactory, Searchable;
 
     public function tags(): BelongsToMany
