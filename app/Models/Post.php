16e849a5d5a0bbf8bb2f73785a70ec5f89af23e4
AgungDH <agungdh@live.com>
2025-06-20 09:32:11 +0700

    Integrate Laravel Scout for improved search capabilities; update composer dependencies

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,11 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Laravel\Scout\Searchable;
 
 class Post extends Model
 {
     /** @use HasFactory<\Database\Factories\PostFactory> */
-    use HasFactory;
+    use HasFactory, Searchable;
 
     public function tags(): BelongsToMany
     {
