44a8ee9a7a9bc482617304ded5ac8a4bc111a24d
AgungDH <agungdh@live.com>
2025-06-20 22:09:28 +0700

    Remove unused toSearchableArray method from Post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -23,16 +23,6 @@ class Post extends Model
         return $this->belongsToMany(Category::class);
     }
 
-    public function toSearchableArray(): array
-    {
-        $array = $this->toArray();
-
-        $array['tags'] = $this->tags->pluck('tag')->toArray();
-        $array['categories'] = $this->categories->pluck('category')->toArray();
-
-        return $array;
-    }
-
     public function shouldBeSearchable(): bool
     {
         return $this->status === 'published';
