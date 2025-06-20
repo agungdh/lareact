72fc1d8d8c1b22a15dc0bb56ec2769d8e9252240
AgungDH <agungdh@live.com>
2025-06-20 21:49:05 +0700

    Add Queue configuration and update environment settings for Redis and MeiliSearch

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,11 +6,12 @@ use Database\Factories\PostFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Laravel\Scout\Searchable;
 
 class Post extends Model
 {
     /** @use HasFactory<PostFactory> */
-    use HasFactory;
+    use HasFactory, Searchable;
 
     public function tags(): BelongsToMany
     {
