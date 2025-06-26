ccc2384ee362447cd7d4ae6e2bb7f919c71c8ae5
AgungDH <agungdh@live.com>
2025-06-26 13:15:02 +0700

    add diff script

--- diff.sh
+++ diff.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
+# Mengecek apakah ada parameter yang diberikan untuk pesan
+message="${1:-generate commit message}"
+
 # Mendapatkan informasi tentang perubahan, penambahan, dan penghapusan file
 status_output=$(git status --short)
 diff_output=$(git diff)
 
 # Gabungkan hasil status dan diff
 if [ -z "$diff_output" ] && [ -z "$status_output" ]; then
-  echo -e "aku ganteng dan aku bangga" | xclip -selection clipboard
+  echo -e "$message" | xclip -selection clipboard
 else
-  echo -e "aku ganteng dan aku bangga\n\nStatus Perubahan:\n$status_output\n\nGit Diff:\n$diff_output" | xclip -selection clipboard
+  echo -e "$message\n\nStatus Perubahan:\n$status_output\n\nGit Diff:\n$diff_output" | xclip -selection clipboard
 fi
 
 echo "Git diff, status, and message have been copied to clipboard."
